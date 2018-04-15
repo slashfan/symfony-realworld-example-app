650150a88a1ba3917ba384d421780b7dc688cb86
Nicolas Cabot <nicocabot@gmail.com>
2018-04-15 21:41:51 +0200

    added tests for user resolver

---
+++ tests/Security/UserResolverTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Tests\Security;
+
+use App\Security\UserResolver;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * UserResolverTest.
+ */
+class UserResolverTest extends TestCase
+{
+    /**
+     * @throws \Exception
+     *
+     * @expectedException \Exception
+     */
+    public function testGetCurrentUserWithNullUser()
+    {
+        $storage = $this->getMockBuilder(TokenStorageInterface::class)->getMock();
+        $storage->method('getToken')->willReturn(null);
+
+        /** @var TokenStorageInterface $storage */
+        $resolver = new UserResolver($storage);
+        $resolver->getCurrentUser();
+    }
+
+    /**
+     * @throws \Exception
+     *
+     * @expectedException \Exception
+     */
+    public function testGetCurrentUserWithNonAppUser()
+    {
+        $user = $this->getMockBuilder(UserInterface::class)->getMock();
+
+        $token = $this->getMockBuilder(TokenInterface::class)->getMock();
+        $token->method('getUser')->willReturn($user);
+
+        $storage = $this->getMockBuilder(TokenStorageInterface::class)->getMock();
+        $storage->method('getToken')->willReturn($token);
+
+        /** @var TokenStorageInterface $storage */
+        $resolver = new UserResolver($storage);
+        $resolver->getCurrentUser();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testGetCurrentUserWithAppUser()
+    {
+        $user = $this->getMockBuilder(\App\Entity\User::class)->getMock();
+
+        $token = $this->getMockBuilder(TokenInterface::class)->getMock();
+        $token->method('getUser')->willReturn($user);
+
+        $storage = $this->getMockBuilder(TokenStorageInterface::class)->getMock();
+        $storage->method('getToken')->willReturn($token);
+
+        /** @var TokenStorageInterface $storage */
+        $resolver = new UserResolver($storage);
+        $this->assertSame($user, $resolver->getCurrentUser());
+    }
+}
