6df67c0b28bb6dd28c512965946cf90f58b06fe0
Nicolas Cabot <nicocabot@gmail.com>
2019-01-18 21:56:49 +0100

    updated dependencies, fixed tags input type for symfony 4.2.2

--- src/Form/Type/TagsInputType.php
+++ src/Form/Type/TagsInputType.php
@@ -10,6 +10,7 @@ use Symfony\Bridge\Doctrine\Form\DataTransformer\CollectionToArrayTransformer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * TagsInputType.
@@ -38,6 +39,14 @@ class TagsInputType extends AbstractType
             ->addModelTransformer(new CollectionToArrayTransformer(), true)
             ->addModelTransformer(new TagArrayToStringTransformer($this->tagRepository), true)
         ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefault('multiple', true);
     }
 
     /**
