324233f0e059e7765f89051f81db749e785c4212
Nicolas Cabot <nicocabot@gmail.com>
2021-02-08 09:58:08 +0100

    fixed specs test runner for npm 7

--- spec/api-spec-test-runner.sh
+++ spec/api-spec-test-runner.sh
@@ -8,7 +8,7 @@ USERNAME=${USERNAME:-u`date +%s`}
 EMAIL=${EMAIL:-$USERNAME@mail.com}
 PASSWORD=${PASSWORD:-password}
 
-npx newman run $SCRIPTDIR/conduit.postman_collection.json \
+npx --yes newman run $SCRIPTDIR/conduit.postman_collection.json \
   --delay-request 500 \
   --global-var "APIURL=$APIURL" \
   --global-var "USERNAME=$USERNAME" \
