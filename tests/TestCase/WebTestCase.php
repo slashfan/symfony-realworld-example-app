c125b3b5217128899a82ae57039a202e2b3933f0
Nicolas Cabot <nicocabot@gmail.com>
2021-08-07 10:43:35 +0200

    migrated to new authenticator based security

--- tests/TestCase/WebTestCase.php
+++ tests/TestCase/WebTestCase.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Tests\TestCase;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTestCase;
 
@@ -16,14 +19,20 @@ class WebTestCase extends BaseWebTestCase
         ]);
     }
 
-    protected function createAuthenticatedApiClient(
-        string $user = 'user1@conduit.tld',
-        string $password = 'password'
-    ): KernelBrowser {
+    protected function createAuthenticatedApiClient(string $user = 'user1@conduit.tld'): KernelBrowser
+    {
+        $user = static::getContainer()->get(UserRepository::class)->findOneBy(['email' => $user]);
+
+        if (!$user instanceof User) {
+            throw new \InvalidArgumentException('User not found.');
+        }
+
+        $token = static::getContainer()->get(JWTTokenManagerInterface::class)->create($user);
+        static::ensureKernelShutdown();
+
         return static::createClient([], [
             'CONTENT_TYPE' => 'application/json',
-            'PHP_AUTH_USER' => $user,
-            'PHP_AUTH_PW' => $password,
+            'HTTP_AUTHORIZATION' => 'Token ' . $token,
         ]);
     }
 }
