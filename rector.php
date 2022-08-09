025c84f2684c116820b6db33992f2f239cb5b672
Nicolas Cabot <nicocabot@gmail.com>
2022-08-09 18:56:31 +0200

    upgraded to php 8.1

--- rector.php
+++ rector.php
@@ -2,14 +2,35 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
 use Rector\Symfony\Set\SymfonySetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::CACHE_DIR, 'var/rector');
-    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
-    $parameters->set(Option::SYMFONY_CONTAINER_XML_PATH_PARAMETER, __DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
-    $containerConfigurator->import(SymfonySetList::SYMFONY_CODE_QUALITY);
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->parallel();
+    $rectorConfig->phpVersion(PhpVersion::PHP_81);
+    $rectorConfig->cacheDirectory('var/rector');
+    $rectorConfig->symfonyContainerXml(__DIR__ . '/var/cache/dev/AppKernelDevDebugContainer.xml');
+    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon.dist');
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/migrations',
+        __DIR__ . '/tests',
+    ]);
+    $rectorConfig->skip([
+        AnnotationToAttributeRector::class,
+        // __DIR__ . '/src',
+        // __DIR__ . '/src/*/Tests/*',
+        // __DIR__ . '/src/ComplicatedFile.php',
+        // SimplifyIfReturnBoolRector::class,
+        // SimplifyIfReturnBoolRector::class => [
+        //    __DIR__ . '/src/ComplicatedFile.php',
+        //    __DIR__ . '/src',
+        //    __DIR__ . '/src/*/Tests/*',
+        // ],
+    ]);
+    $rectorConfig->sets([
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+    ]);
 };
---
+++ tests/phpstan.doctrine.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+(new Dotenv())->bootEnv(__DIR__ . '/../.env');
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel->boot();
+
+return $kernel->getContainer()->get('doctrine')->getManager();
